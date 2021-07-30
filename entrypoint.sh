4dfa11e85b8b40e785a8ba792f207eb9155154e0
Sphelele <sphemicah@gmail.com>
2021-07-30 22:26:44 +0200

    Updated entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 hub checkout ${${REPO_BRANCH}:-master}
 VERSION_NAME=`grep -oP 'versionName "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle`
-hub release create -a  ./${APP_FOLDER}/build/outputs/apk/release/app-release.apk -m "App Release - ${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
+hub release create -a ${APP_FOLDER}/build/outputs/apk/release/app-release.apk -m "App Release - ${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
